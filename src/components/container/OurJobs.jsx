925b64dd870e9b5f841b001d2e40c85a64e038a6
ValentinoIlardo <valentinoilardo@gmail.com>
2022-11-16 20:48:17 -0300

    edite los videos por gifs en secc de trabajos
    edite algunas clases de OurJobs

--- src/components/container/OurJobs.jsx
+++ src/components/container/OurJobs.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import '../../styles/JobsStyles/OurJobsStyles.css';
-import vid1 from '../../assets/videos/videoAbout1.mp4';
+import vid1 from '../../assets/gifs/aboutUs-1.gif';
+import vid2 from '../../assets/gifs/aboutUs-2.gif';
+import vid3 from '../../assets/gifs/aboutUs-3.gif';
+import vid4 from '../../assets/gifs/aboutUs-4.gif';
+import vid5 from '../../assets/gifs/aboutUs-5.gif';
+import vid6 from '../../assets/gifs/aboutUs-6.gif';
+import { ImageGrid } from 'react-fb-image-video-grid';
 
 function OurJobs() {
 
@@ -10,14 +16,14 @@ function OurJobs() {
   return (
     <section className='jobs-main-container'>
         <h2>{t("ourJobs.title")}</h2>
-        <div className='jobs-videos-container'>
-            <video className='video' src={vid1} width='400' height='400' autoPlay />
-              <video className='video' src={vid1} width='400' height='400' autoPlay />
-              <video className='video' src={vid1} width='400' height='400' autoPlay />
-              <video className='video' src={vid1} width='400' height='400' autoPlay />
-              <video className='video' src={vid1} width='400' height='400' autoPlay />
-              <video className='video' src={vid1} width='400' height='400' autoPlay />
-        </div>
+          <ImageGrid className= "ImageGrid">
+            <img className='video' src={vid1}  />
+            <img className='video' src={vid5}  />
+            <img className='video' src={vid2}  />
+            <img className='video' src={vid4}  />
+            <img className='video' src={vid3}  />
+            {/*<img className='video' src={vid6}  />*/}
+          </ImageGrid>
     </section>
   )
 }
