1e5815cb27f002a94cfcb216b3d96eb8c9aac89f
AxelMrak <axelmrak@gmail.com>
2022-11-17 18:20:53 -0300

    Agregue seccion Instagram

---
+++ src/components/container/Instagram.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Instagram() {
+    return (
+        <div>
+            <div className="embedsocial-hashtag" data-ref="e500e8a2c556976dd14a3f93b4eadde0fd72bbfe"> <a className="feed-powered-by-es feed-powered-by-es-feed-new" href="https://embedsocial.com/social-media-aggregator/" target="_blank" title="Widget by EmbedSocial"> Widget by EmbedSocial<span>→</span> </a> </div>
+            <script>
+                {
+                    (function (d, s, id) { var js; if (d.getElementById(id)) { return; } js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js); }(document, "script", "EmbedSocialHashtagScript"))
+                } 
+            </script>
+        </div>
+    )
+};
+
+export default Instagram;
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,13 +3,16 @@ import '../../styles/HomePageStyles/HomePageStyles.css';
 import About from '../../components/container/About';
 import Main from '../../components/container/Main';
 import OurJobs from '../../components/container/OurJobs';
+import Instagram from '../../components/container/Instagram';
 
 function HomePage() {
+
   return (
     <div className='home-main-container'>
         <Main/>
         <About/>
-       <OurJobs/>
+        <OurJobs/>
+        <Instagram/>
     </div>
   )
 }
