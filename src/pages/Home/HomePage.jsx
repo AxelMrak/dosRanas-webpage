a53dfa908258477e3b65f7398d7b67c62bb6b4b1
AxelMrak <axelmrak@gmail.com>
2022-11-16 23:22:14 -0300

    Repare bugs con IG Widget

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,15 +3,13 @@ import '../../styles/HomePageStyles/HomePageStyles.css';
 import About from '../../components/container/About';
 import Main from '../../components/container/Main';
 import OurJobs from '../../components/container/OurJobs';
-import InstagramWidget from '../../components/pure/InstagramWidget';
 
 function HomePage() {
   return (
     <div className='home-main-container'>
         <Main/>
         <About/>
-        <OurJobs/>
-        <InstagramWidget/>
+       <OurJobs/>
     </div>
   )
 }
