969924fdfa5cb3e802e9dfa82d2317b7b91b731a
AxelMrak <axelmrak@gmail.com>
2022-11-17 10:26:07 -0300

    Dependencias actualizadas

--- src/components/container/OurJobs.jsx
+++ src/components/container/OurJobs.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { ImageGrid } from 'react-fb-image-video-grid';
 import '../../styles/JobsStyles/OurJobsStyles.css';
 import vid1 from '../../assets/gifs/aboutUs-1.gif';
 import vid2 from '../../assets/gifs/aboutUs-2.gif';
@@ -8,7 +7,7 @@ import vid3 from '../../assets/gifs/aboutUs-3.gif';
 import vid4 from '../../assets/gifs/aboutUs-4.gif';
 import vid5 from '../../assets/gifs/aboutUs-5.gif';
 import vid6 from '../../assets/gifs/aboutUs-6.gif';
-
+import { ImageGrid } from 'react-fb-image-video-grid';
 
 function OurJobs() {
 
